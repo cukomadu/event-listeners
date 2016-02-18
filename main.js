d5ceed3a5d1f968936dd9971ee3717caf6ca8ffc
Justin Richards <justinrichards@Justins-MacBook-Air.local>
2016-02-17 19:09:47 -0600

    added cursor tracking to gifs

--- main.js
+++ main.js
@@ -1,2 +1,22 @@
 // GO!
 
+
+
+// BONUS 
+var input = document.querySelector("#add-guest-bonus input")
+var guestList = document.querySelector("#add-guest-bonus ul")
+
+var addGuest = function(e) {
+	console.log(e.keyCode)
+	if (e.keyCode === 13) {
+		var newGuestItem = document.createElement('li')
+		newGuestItem.innerHTML = e.target.value + "<button>X</button>"
+		newGuestItem.classList.add('guest')
+		newGuestItem.querySelector('button').addEventListener('click',function() {
+			guestList.removeChild(newGuestItem)
+		})
+		guestList.appendChild(newGuestItem)
+		e.target.value = ''
+	}
+}
+input.addEventListener('keypress',addGuest)
