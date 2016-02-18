e29d29f712a287d90dc47c0f8c2ad38971af6fb9
Justin Richards <justinrichards@Justins-MacBook-Air.local>
2016-02-17 18:55:48 -0600

    initial commit

--- project-setup.js
+++ project-setup.js
@@ -29,41 +29,20 @@ function generatePromptAndDemoHTML(obj, index){
 
 var tasks = [
   { 
-    html_id: "pig-latin",
-    task: "Make each element in the list read backwards.",
-    link: "gifs/tasks.gif"
-  },
-  {
-    html_id: "blow-up",
-    task: "By manipulating the <code> &lt;div class='circle'&gt; </code> DOM element in JS, make the circle double height and width until its width is greater-than/equal-to 320px." +
-          "</br></br>"+
-          "At that point make it reduce back to its original size of 40px x 40px ",
-    link: "gifs/circle-red.gif"
-  },
-  {  
-    html_id: "reverse-squares",
-    task: "Flip the squares to opposite order",
-    link: "gifs/flip.gif"
-  },
-  {
-    html_id: "color-circle",
-    task: "Change the color of the circle from white to black, and vice-versa",
-    link: "gifs/circle-bw.gif"
-  },
-  {html_id: "thanks",
-  task: "Cause the browser to produce an alert box that says 'yowch! don't click me so hard!'",
-  link: "gifs/yowch.gif"},
-  {html_id: "double",
-  task: "Each click of the button should double the value of the number below.",
-  link: 'gifs/double.gif'
-  },
-  {html_id: "remove",
-  task: "Remove each element of the list below that has the class 'inactive'.",
-  link: 'gifs/remove.gif'
+    html_id: "hide-nav",
+    task: "Add a button into the answer-box that, when clicked, will toggle the visibility of the nav menu. The easiest way is probably to change the class on the nav menu. (Check the css file.)",
+    link: "gifs/toggle-vis.gif"
   },  
-  {html_id: "cycle-image",
-  task: "By increasing the number at the end of this image's 'src' attribute, you can cycle through different city photos. To cycle forever, you may need to use the % operator!",
-  link: 'gifs/cities.gif'}
+  { 
+    html_id: "add-guest",
+    task: "When the user hits enter while focus is on the input, the content of the input should be added to the list below.",
+    link: "gifs/guestList.gif"
+  },
+  { 
+    html_id: "add-guest-bonus",
+    task: "Each added guest should come with a remove button. Clicking that button should remove the guest from the list. This is an adventure mode problem. Your homework can be considered technically complete if this is not solved.",
+    link: "gifs/guestListBonus.gif"
+  },
 ]
 
 function findEl(els, fn){
@@ -78,7 +57,6 @@ function findEl(els, fn){
       break;
     } 
   }
-
   return foundEl
 }
 
